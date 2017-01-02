fa4b288b861cf9b72e8f198443e0ab828d53d17e
Ajibade Adebayo <asqwrd@gmail.com>
2017-01-02 02:33:04 -0800

    fixed server

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 
 const app = require('express')();
-const https = require('https');
+const server = require('https').createServer(lex.httpsOptions, lex.middleware(app));
 const got = require('got');
+function approveDomains(opts, certs, cb) {
+  // This is where you check your database and associated
+  // email addresses with domains and agreements and such
+
+
+  // The domains being approved for the first time are listed in opts.domains
+  // Certs being renewed are listed in certs.altnames
+  if (certs) {
+    opts.domains = certs.altnames;
+  }
+  else {
+    opts.email = 'asqwrd@gmail.com';
+    opts.agreeTos = true;
+  }
+
+  cb(null, { options: opts, certs: certs });
+}
+
+
 var lex = require('letsencrypt-express').create({
-
-  server: 'staging'
-
-, email: 'asqwrd@gmail.com'
-
-, agreeTos: true
-
-, approveDomains: [ 'ajibade.me' ]
+  server: 'staging',
+  challenges: { 'http-01': require('le-challenge-fs').create({ webrootPath: '/tmp/acme-challenges' }) },
+  store: require('le-store-certbot').create({ webrootPath: '/tmp/acme-challenges' }),
+  approveDomains: approveDomains
 
 })
+
+require('http').createServer(lex.middleware(require('redirect-https')())).listen(80, function () {
+  console.log("Listening for ACME http-01 challenges on", this.address());
+});
+
 var server = require('http').createServer(app);
 
 const redirect = (url, res) => got(url, {json: true})
@@ -60,7 +80,6 @@ app.get('/api/schedule', (req, res) => {
 
 //lex.onRequest = app;
 
-lex.listen(3001, function () {
-  var protocol = ('requestCert' in this) ? 'https': 'http';
-  console.log("Listening at " + protocol + '://localhost:' + this.address().port);
+server.listen(3001, function () {
+  console.log("Listening for ACME tls-sni-01 challenges and serve app on", this.address());
 });
